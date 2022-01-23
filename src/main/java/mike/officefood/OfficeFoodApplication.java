fcef6701199e95c1e8c7303f44969279438597ee
i.makeev <i.makeev@krista.ru>
2022-01-23 23:58:24 +0300

    offo-1: создать скелет проекта

--- src/main/java/mike/officefood/OfficeFoodApplication.java
+++ src/main/java/mike/officefood/OfficeFoodApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OfficefoodApplication {
+public class OfficeFoodApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OfficefoodApplication.class, args);
+		SpringApplication.run(OfficeFoodApplication.class, args);
 	}
 
 }
